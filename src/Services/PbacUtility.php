990ac20c2ebf54faf80c6e0ce567f502f7f3ea65
Manpreet <m.singh@kreatif.it>
2025-10-30 07:56:03 +0100

    wip: update migrations

--- src/Services/PbacUtility.php
+++ src/Services/PbacUtility.php
@@ -2,61 +2,46 @@
 
 namespace Pbac\Services;
 
-
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\ColumnDefinition;
 use Illuminate\Support\Facades\Config;
 
 class PbacUtility
 {
-
     /**
-     * Get the User Model class name from configuration.
-     *
      * @return class-string<\Illuminate\Foundation\Auth\User>
      */
     public function getUserModel(): string
     {
-        $userModelClass = Config::get('pbac.user_model', \App\Models\User::class);
-        return $userModelClass;
+        return Config::get('pbac.user_model', \App\Models\User::class);
     }
 
     public function getAuthenticatableTableName(): string
     {
-        // $userModelClass = $this->getUserModel();
-        // /** @var Model&Authenticatable $userModelInstance */
-        // $userModelInstance = new $userModelClass();
-        // return $userModelInstance->getTable();
-        return config('pbac.users.table') ?? (new ($this->getUserModel()))->getTable();
+        return Config::get('pbac.users.table') ?? (new ($this->getUserModel()))->getTable();
     }
 
     public function getAuthenticatableKeyName(): string
     {
-        // $userModelClass = $this->getUserModel();
-        // /** @var Model&Authenticatable $userModelInstance */
-        // $userModelInstance = new $userModelClass();
-        // return $userModelInstance->getKeyName();
-        return config('pbac.users.key') ?? (new ($this->getUserModel()))->getKeyName();
+        return Config::get('pbac.users.key') ?? (new ($this->getUserModel()))->getKeyName();
     }
 
     public function getAuthenticatableKeyType(): string
     {
-        // $userModelClass = $this->getUserModel();
-        // /** @var Model&Authenticatable $userModelInstance */
-        // $userModelInstance = new $userModelClass();
-        // $tableName = $userModelInstance->getTable();
-        // $keyName = $userModelInstance->getKeyName();
-        // return \Illuminate\Support\Facades\Schema::getColumnType($tableName, $keyName);
-        return config('pbac.users.key_type', 'unsignedBigInteger') ?? (new ($this->getUserModel()))->getKeyType();
+        return strtolower(Config::get('pbac.users.key_type', 'unsignedbiginteger'));
     }
 
     public function getAuthenticatableKeyLength(): ?int
     {
-        return config('pbac.users.key_length', null);
+        return Config::get('pbac.users.key_length');
     }
 
-    public function addUserForeignKey(Blueprint $table, string $column, bool $nullable = false, string $onDelete = 'cascade'): ColumnDefinition
-    {
+    public function addUserForeignKey(
+        Blueprint $table,
+        string $column,
+        bool $nullable = false,
+        string $onDelete = 'cascade'
+    ): ColumnDefinition {
         $usersTable = $this->getAuthenticatableTableName();
         $usersKey   = $this->getAuthenticatableKeyName();
 
@@ -68,23 +53,28 @@ class PbacUtility
                 $col = $table->ulid($column);
                 break;
             case 'string':
-                $col = $table->string($column, $this->getAuthenticatableKeyLength());
+                $length = $this->getAuthenticatableKeyLength() ?? 191;
+                $col = $table->string($column, $length);
                 break;
-            default: // bigint
+            case 'biginteger':
+            case 'unsignedbiginteger':
+            default:
                 $col = $table->unsignedBigInteger($column);
                 break;
         }
 
-        if ($nullable) $col->nullable();
+        if ($nullable) {
+            $col->nullable();
+        }
 
         $table->foreign($column)
               ->references($usersKey)
               ->on($usersTable)
               ->when($onDelete === 'cascade', fn ($c) => $c->cascadeOnDelete())
               ->when($onDelete === 'set null', fn ($c) => $c->nullOnDelete());
+
+        $table->index($column);
+
         return $col;
     }
-
-
 }
-
