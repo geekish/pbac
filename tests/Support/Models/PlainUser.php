fd801f2bce83cda0371c24866fbb9f31420d8036
Hannah Chartier <hannah@sent.com>
2026-05-14 09:43:40 -0500

    Support Laravel 13

--- tests/Support/Models/PlainUser.php
+++ tests/Support/Models/PlainUser.php
@@ -2,19 +2,11 @@
 
 namespace Pbac\Tests\Support\Models;
 
-
-
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Foundation\Auth\User;
 
-class PlainUser extends Model implements AuthenticatableContract, AuthorizableContract
+class PlainUser extends User
 {
-    use Authenticatable;
-    use Authorizable;
     use HasFactory;
 
     protected $fillable = ['email', 'password', 'name', 'is_super_admin'];
